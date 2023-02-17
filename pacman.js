e3da1aa39376cea87c5498d70055d6e97af0b4b2
joshuapent <joshdavidpent@gmail.com>
2023-02-17 13:36:34 -0600

    Set up some basic CSS

--- pacman.js
+++ pacman.js
@@ -1,3 +1,8 @@
-const ghosts = ['images/ghost1.png', 'images/ghost2.png', 'images/ghost3.png', 'images/ghost4.png'];
-const pacman = ['images/pacman.png']
-const arena = document.getElementById('');
+const ghostsImg = ['images/ghost1.png', 'images/ghost2.png', 'images/ghost3.png', 'images/ghost4.png'];
+const pacmanImg = ['images/pacman.png']
+const gameArea = document.getElementById('GameArea');
+class character {
+    constructor(name) {
+        this.name = name,
+    }
+}
