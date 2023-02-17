2e5f3acdff0a05f9e634375b2b21bc0d90ef657f
joshuapent <joshdavidpent@gmail.com>
2023-02-17 14:57:07 -0600

    added a few more sprites

--- pacman.js
+++ pacman.js
@@ -1,11 +1,12 @@
-const ghostsImg = ['images/ghost1.png', 'images/ghost2.png', 'images/ghost3.png', 'images/ghost4.png'];
-const pacmanImg = ['images/pacman.png']
+const ghostsImg = ['images/blueghost1.png', 'images/brownghost1.png', 'images/redghost1.png', 'images/pinkghost1.png'];
+const pacmanImg = ['images/pacman1.png']
 const gameArea = document.getElementById('GameArea');
 const pacman = document.getElementById('pacman');
 const ghost1 = document.getElementById('ghost1');
 const ghost2 = document.getElementById('ghost2');
 const ghost3 = document.getElementById('ghost3');
 const ghost4 = document.getElementById('ghost4');
+timer
 pacman.setAttribute("src", pacmanImg[0]);
 ghost1.setAttribute("src", ghostsImg[0]);
 ghost2.setAttribute("src", ghostsImg[1]);
