599614a378e3c7b605144c91ecc16567140dbf7f
joshuapent <joshdavidpent@gmail.com>
2023-02-17 13:28:44 -0600

    Set up HTML sprite elements to link to JS

--- pacman.js
+++ pacman.js
@@ -1,2 +1,3 @@
 const ghosts = ['images/ghost1.png', 'images/ghost2.png', 'images/ghost3.png', 'images/ghost4.png'];
 const pacman = ['images/pacman.png']
+const arena = document.getElementById('');
