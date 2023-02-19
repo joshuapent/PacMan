2b3eff5ea364c717f64e6b72a32d1f952a2145f8
joshuapent <joshdavidpent@gmail.com>
2023-02-18 23:11:04 -0600

    Fixed some movement bugs

--- pacman.js
+++ pacman.js
@@ -16,45 +16,45 @@ let pacmanvar = 0;
 const pacmanmoves = setInterval(function () {
     if (pacmanvar < pacman.spriteNum) {
         game.clearRect(0,0,640,800)
-        game.drawImage(sprites, pacmanvar*32, 0, 32, 32, 200, 100, 32, 32)
+        game.drawImage(sprites, pacmanvar*32, 0, 32, 32, xAxis, yAxis, 32, 32)
         pacmanvar++;
     } else {
     game.clearRect(0,0,640,800)
-    game.drawImage(sprites, 32, 0, 32, 32, 200, 100, 32, 32)
+    game.drawImage(sprites, 32, 0, 32, 32, xAxis, yAxis, 32, 32)
     pacmanvar = 0;
     }
-},1000); //100 is the sweet spot
+},100); //100 is the sweet spot
 
 
 let yAxis = gameAreaSetup.height/2; //using gameAreaSetup allows for this to change with arena size
 let xAxis = gameAreaSetup.width/2; 
 document.addEventListener('keydown', (direction) => {
     if (direction.code == 'KeyW' || direction.code == "ArrowUp") {
-        if (yAxis < gameAreaSetup.height) {
-            if (yAxis >= gameAreaSetup.height-pacman.movement) yAxis = gameAreaSetup.height; 
-            else yAxis += pacman.movement; //using .movement allows game speed to be changed easily
-        }
-        console.log(yAxis)
-    }
-    else if (direction.code == 'KeyS' || direction.code == "ArrowDown") {
         if (yAxis > 0) {
             if (yAxis <= pacman.movement) yAxis -= yAxis;
             else yAxis -= pacman.movement; 
             }
         console.log(yAxis)
+    }
+    else if (direction.code == 'KeyS' || direction.code == "ArrowDown") {
+        if (yAxis < gameAreaSetup.height-pacman.dim) {
+            if (yAxis >= gameAreaSetup.height-pacman.movement) yAxis = gameAreaSetup.height; 
+            else yAxis += pacman.movement; //using .movement allows game speed to be changed easily
+        }
+        console.log(yAxis)
     } 
     else if (direction.code == 'KeyA' || direction.code == "ArrowLeft") {
-        if (xAxis < gameAreaSetup.width) {
-            if (xAxis >= gameAreaSetup.width-pacman.movement) xAxis = gameAreaSetup.width; 
-            else xAxis += pacman.movement;
-        } // gameAreaSetup.width/height allows for this function to be flexible to arena changes
-        console.log(xAxis)
-    } 
-    else if (direction.code == 'KeyD' || direction.code == "ArrowRight") {
         if (xAxis > 0) {
             if (xAxis <= pacman.movement) xAxis -= xAxis;
             else xAxis -= pacman.movement; 
         }
+        console.log(xAxis)
+    } 
+    else if (direction.code == 'KeyD' || direction.code == "ArrowRight") {
+        if (xAxis < gameAreaSetup.width-pacman.dim) {
+            if (xAxis >= gameAreaSetup.width-pacman.movement) xAxis = gameAreaSetup.width; 
+            else xAxis += pacman.movement;
+        } // gameAreaSetup.width/height allows for this function to be flexible to arena changes
         console.log(xAxis)
     }
 });
