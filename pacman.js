366c711fb78b037b5606037637fbd7e355d1cc38
joshuapent <joshdavidpent@gmail.com>
2023-02-19 17:52:44 -0600

    Moved code to consolidated location for cleanliness

--- pacman.js
+++ pacman.js
@@ -22,23 +22,39 @@ class Sprite {
         }
     animate() { //makes the sprite animated, ex: Pacman moves his mouth.
         if (this.direction === "up") {
+            if (pacman.yAxis > 0) {
+            if (pacman.yAxis <= pacman.movement) pacman.yAxis -= pacman.yAxis;
+            else pacman.yAxis -= pacman.movement; 
             game.clearRect(this.xAxis,this.yAxis+this.movement,32,32)
-            this.directionI = this.column+2;
+            this.directionI = this.column+1;
+            }
         }
         else if (this.direction === "down") {
+            if (pacman.yAxis < gameAreaSetup.height-pacman.dim) {
+            if (pacman.yAxis >= gameAreaSetup.height-pacman.movement) pacman.yAxis = gameAreaSetup.height; 
+            else pacman.yAxis += pacman.movement; //using .movement allows game speed to be changed easily
             game.clearRect(this.xAxis,this.yAxis-this.movement,32,32)
-            this.directionI = this.column+6;
+            this.directionI = this.column+5;
+            }
         }
         else if (this.direction === "left") {
+            if (pacman.xAxis > 0) {
+            if (pacman.xAxis <= pacman.movement) pacman.xAxis -= pacman.xAxis;
+            else pacman.xAxis -= pacman.movement; 
             game.clearRect(this.xAxis+this.movement,this.yAxis,32,32)
-            this.directionI = this.column+4;
+            this.directionI = this.column+3;
+            }
         }
         else if (this.direction === "right") {
+            if (pacman.xAxis < gameAreaSetup.width-pacman.dim) {    
+            if (pacman.xAxis >= gameAreaSetup.width-pacman.movement) pacman.xAxis = gameAreaSetup.width; 
+            else pacman.xAxis += pacman.movement;
             game.clearRect(this.xAxis-this.movement,this.yAxis,32,32)
-            this.directionI = this.column;
+            this.directionI = this.column-1;
+            }
         };
         if(this.column < this.column + this.spriteNum) {
-            game.drawImage(sprites, (this.column+this.spriteNum+this.directionI) * this.dim, this.row * this.dim, 32, 32, this.xAxis, this.yAxis, 32, 32)
+            game.drawImage(sprites, (this.spriteNum+this.directionI) * this.dim, this.row * this.dim, 32, 32, this.xAxis, this.yAxis, 32, 32)
             this.spriteNum--;
             console.log(this.spriteNum)
         } else {
@@ -47,16 +63,16 @@ class Sprite {
                 this.spriteNum = 2
             }
             else {
-                game.drawImage(sprites, (this.column+this.spriteNum+this.directionI) * this.dim, this.row * this.dim, 32, 32, this.xAxis, this.yAxis, 32, 32)
+                game.drawImage(sprites, (this.spriteNum+this.directionI) * this.dim, this.row * this.dim, 32, 32, this.xAxis, this.yAxis, 32, 32)
                 this.spriteNum = 2;
                 console.log(this.spriteNum)
             }
         } 
-        setTimeout(100)
+        // setTimeout(100)
     }
 
 };
-const pacman = new Sprite("pacman", 0, 0, 2, 8, 200, 200, "up");
+const pacman = new Sprite("pacman", 0, 1, 2, 8, 200, 200, "up");
 let pacmanvar = 0;
 
 // let yAxis = gameAreaSetup.height/2; //using gameAreaSetup allows for this to change with arena size
@@ -67,37 +83,17 @@ let pacmanvar = 0;
 document.addEventListener('keydown', (direction) => {
     pacman.animate();
     if (direction.code == 'KeyW' || direction.code == "ArrowUp") {
-        if (pacman.yAxis > 0) {
-            if (pacman.yAxis <= pacman.movement) pacman.yAxis -= pacman.yAxis;
-            else pacman.yAxis -= pacman.movement; 
-            console.log(pacman.yAxis)
             pacman.direction = "up" 
             }
-    }
     else if (direction.code == 'KeyS' || direction.code == "ArrowDown") {
-        if (pacman.yAxis < gameAreaSetup.height-pacman.dim) {
-            if (pacman.yAxis >= gameAreaSetup.height-pacman.movement) pacman.yAxis = gameAreaSetup.height; 
-            else pacman.yAxis += pacman.movement; //using .movement allows game speed to be changed easily
             pacman.direction = "down" 
         }
-        console.log(pacman.yAxis)
-    } 
     else if (direction.code == 'KeyA' || direction.code == "ArrowLeft") {
-        if (pacman.xAxis > 0) {
-            if (pacman.xAxis <= pacman.movement) pacman.xAxis -= pacman.xAxis;
-            else pacman.xAxis -= pacman.movement; 
             pacman.direction = "left" 
         }
-        console.log(pacman.xAxis)
-    } 
     else if (direction.code == 'KeyD' || direction.code == "ArrowRight") {
-        if (pacman.xAxis < gameAreaSetup.width-pacman.dim) {
-            if (pacman.xAxis >= gameAreaSetup.width-pacman.movement) pacman.xAxis = gameAreaSetup.width; 
-            else pacman.xAxis += pacman.movement;
             pacman.direction = "right"
-        } // gameAreaSetup.width/height allows for this function to be flexible to arena changes
-        console.log(pacman.xAxis)
-    }
+        }// gameAreaSetup.width/height allows for this function to be flexible to arena changes
 });
 
 
