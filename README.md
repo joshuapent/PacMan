b7f7b7ed48bca3341a2f4070833d428fee8836f7
joshuapent <joshdavidpent@gmail.com>
2023-02-18 21:16:37 -0600

    Added sprites to the canvas, working on movement

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PacMan
 This is a pacman game
+
+Source for assistance on canvas manipulation: 
+https://spicyyoghurt.com/tutorials/html5-javascript-game-development/images-and-sprite-animations
+
+No code was copied, but the process was learned from there.
--- pacman.js
+++ pacman.js
@@ -1,23 +1,34 @@
 const sprites = new Image();
 const gameAreaSetup = document.getElementById('GameArea');
 const game = gameAreaSetup.getContext("2d");
+class Sprite {
+    constructor(row, column, spriteNum, radius) {
+        this.row = row;
+        this.column = column;
+        this.spriteNum = spriteNum;
+    }
+    dim = 32; // dim = dimensions
+    hitbox = 15; //hitbox is slightly less than half dim because of blank space around sprites
+};
+const pacman = new Sprite(1, 3, 3);
 
-// const pacman = {
-//     width: 16,
-//     height: 16,
-//     row: 1, 
-//     column: 3
-// }
+let randomvar = 0;
+const pacmanmoves = setInterval(function () {
+    if (randomvar < pacman.spriteNum) {
+        game.clearRect(0,0,640,800)
+        game.drawImage(sprites, randomvar*32, 0, 32, 32, 200, 100, 32, 32)
+        randomvar++;
+        console.log(randomvar)
+    } else {
+    game.clearRect(0,0,640,800)
+    game.drawImage(sprites, 64, 0, 32, 32, 200, 100, 32, 32)
+    randomvar = 0;
+    }
+},1000); //100 is the sweet spot
 
-// game.drawImage(sprites, 3*16, 16, 16, 
-//     16, 10, 30, 16, 16);
-let row = 0;
-let column = 0;
 
-sprites.onload = function() {
-    game.drawImage(sprites, column*32, row*32, 32, 32, 200, 100, 32, 32);
-
+document.addEventListener('keydown', (direction) => {
+    if (direction.keycode == 38) return console.log('up')
 }
-
-
+);
 sprites.src = "images/Pacman.png";
