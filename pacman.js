7f48f37ac70d79d18246821ca2cb7b7b5de8a5e8
joshuapent <joshdavidpent@gmail.com>
2023-02-18 19:17:02 -0600

    Found out about how to utilize sprite lists, created a sprite sheet

--- pacman.js
+++ pacman.js
@@ -1,46 +1,29 @@
-const images = {
-    blue: ['images/blueghost1.png', 'images/blueghost2.png'],
-    brown: ['images/brownghost1.png', 'images/brownghost2.png'],
-    red: ['images/redghost1.png', 'images/redghost2.png'],
-    pink: ['images/pinkghost1.png', 'images/pinkghost2.png'],
-    dark: ['images/darkghost1A.png', 'images/darkghost2A.png', 'images/darkghost1B.png', 'images/darkghost2B.png'],
-    pacman: ['images/pacman1.png', 'images/pacman2.png', 'images/pacman3.png']
-};
-const ghosts = [
-    document.getElementById('ghost1'), 
-    document.getElementById('ghost2'),
-    document.getElementById('ghost3'),
-    document.getElementById('ghost4')
-];
-const pacman = document.getElementById('pacman');
-const gameArea = document.getElementById('GameArea');
+const sprites = new Image();
+const gameAreaSetup = document.getElementById('GameArea');
+const game = gameAreaSetup.getContext("2d");
 
-let randomvar = 0;
-function pacManMoves() {
-    if (randomvar < images.pacman.length) {
-        pacman.setAttribute("src", images.pacman[randomvar])
-        randomvar++;
-    } else if (randomvar === images.pacman.length) {
-        pacman.setAttribute("src", images.pacman[1]);
-        randomvar -= 3;
-    }
+sprites.onload = function() {
+    game.drawImage(sprites, column*frameWidth, row*frameHeight, frameWidth, frameHeight, 16, 16, frameWidth, frameHeight);
+
 }
-// const pacmanmoves = setInterval(pacManMoves, 1000); //100 is the sweet spot
 
-function ghostMoves () {
-    if (randomvar === 0) {
-        ghosts[0].setAttribute("src", images.red[0])
-        randomvar++;
-    } else if (randomvar === 1) {
-        ghosts[0].setAttribute("src", images.red[1])
-        randomvar--;
-    }
-}
-// const ghostMoveTimer = setInterval(ghostMoves, 300); //100 is the sweet spot
+// const pacman = {
+//     width: 16,
+//     height: 16,
+//     row: 1, 
+//     column: 3
+// }
+
+// game.drawImage(sprites, 3*16, 16, 16, 
+//     16, 10, 30, 16, 16);
+let frameWidth = 12;
+let frameHeight = 13;
+
+// Rows and columns start from 0
+let row = 1;
+let column = 3;
 
 
-class character {
-    constructor(name) {
-        this.name = name
-    }
-}
+
+
+sprites.src = "images/PacManSprites.png";
---
+++ storage.js
@@ -0,0 +1,42 @@
+const images = {
+    blue: ['images/blueghost1.png', 'images/blueghost2.png'],
+    brown: ['images/brownghost1.png', 'images/brownghost2.png'],
+    red: ['images/redghost1.png', 'images/redghost2.png'],
+    pink: ['images/pinkghost1.png', 'images/pinkghost2.png'],
+    dark: ['images/darkghost1A.png', 'images/darkghost2A.png', 'images/darkghost1B.png', 'images/darkghost2B.png'],
+    pacman: ['images/pacman1.png', 'images/pacman2.png', 'images/pacman3.png']
+};
+const ghosts = [
+    document.getElementById('ghost1'), 
+    document.getElementById('ghost2'),
+    document.getElementById('ghost3'),
+    document.getElementById('ghost4')
+];
+const pacman = document.getElementById('pacman')
+const gameAreaSetup = document.getElementById('GameArea');
+const game = gameAreaSetup.getContext("2d");
+
+// let randomvar = 0;
+function pacManMoves() {
+    if (randomvar < images.pacman.length) {
+        pacman.setAttribute("src", images.pacman[randomvar])
+        randomvar++;
+    } else if (randomvar === images.pacman.length) {
+        pacman.setAttribute("src", images.pacman[1]);
+        randomvar -= 3;
+    }
+}
+// const pacmanmoves = setInterval(pacManMoves, 100); //100 is the sweet spot
+
+function ghostMoves () {
+    if (randomvar === 0) {
+        ghosts[0].setAttribute("src", images.red[0])
+        randomvar++;
+    } else if (randomvar === 1) {
+        ghosts[0].setAttribute("src", images.red[1])
+        randomvar--;
+    }
+}
+// const ghostMoveTimer = setInterval(ghostMoves, 300); //100 is the sweet spot
+
+
