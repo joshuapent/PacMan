f87402f8488fefa022788bc17cd710a46dc9ad8c
joshuapent <joshdavidpent@gmail.com>
2023-02-19 10:53:20 -0600

    Having to use a different way to turn sprites

--- pacman.js
+++ pacman.js
@@ -2,18 +2,22 @@ const sprites = new Image();
 const gameAreaSetup = document.getElementById('GameArea');
 const game = gameAreaSetup.getContext("2d");
 class Sprite {
-    constructor(row, column, spriteNum, movement, radius) {
-        this.row = row;
-        this.column = column;
-        this.spriteNum = spriteNum;
+    constructor(row, column, spriteNum, movement, xAxis, yAxis, radius) {
+        this.row = row; // this is for sprite location
+        this.column = column; //this is for sprite location
+        this.spriteNum = spriteNum; //this is how many imgs to cycle between
         this.movement = movement;
+        this.xAxis = xAxis;
+        this.yAxis = yAxis; 
+        this.radius = radius;
     }
     dim = 32; // dim = dimensions
     hitbox = 15; //hitbox is slightly less than half dim because of blank space around sprites
 };
-const pacman = new Sprite(1, 3, 3, 16);
+const pacman = new Sprite(1, 3, 3, 8);
 let pacmanvar = 0;
-const pacmanmoves = setInterval(function () {
+
+function pacmanMoves() {
     if (pacmanvar < pacman.spriteNum) {
         game.clearRect(0,0,640,800)
         game.drawImage(sprites, pacmanvar*32, 0, 32, 32, xAxis, yAxis, 32, 32)
@@ -23,18 +27,21 @@ const pacmanmoves = setInterval(function () {
     game.drawImage(sprites, 32, 0, 32, 32, xAxis, yAxis, 32, 32)
     pacmanvar = 0;
     }
-},100); //100 is the sweet spot
+    setTimeout(100)
+}
 
 
 let yAxis = gameAreaSetup.height/2; //using gameAreaSetup allows for this to change with arena size
 let xAxis = gameAreaSetup.width/2; 
 document.addEventListener('keydown', (direction) => {
+    pacmanMoves();
     if (direction.code == 'KeyW' || direction.code == "ArrowUp") {
         if (yAxis > 0) {
             if (yAxis <= pacman.movement) yAxis -= yAxis;
             else yAxis -= pacman.movement; 
             }
         console.log(yAxis)
+        rotate()
     }
     else if (direction.code == 'KeyS' || direction.code == "ArrowDown") {
         if (yAxis < gameAreaSetup.height-pacman.dim) {
@@ -59,4 +66,13 @@ document.addEventListener('keydown', (direction) => {
     }
 });
 
+function rotate() {
+    game.translate(xAxis, yAxis)
+    game.rotate(-90 * Math.PI/180)
+    game.translate(-xAxis, -yAxis)
+    game.clearRect(0,0,640,800)
+    game.drawImage(sprites, 32, 0, 32, 32, xAxis-20, yAxis-20, pacman.dim, pacman.dim)
+}
+
 sprites.src = "images/Pacman.png";
+
--- storage.js
+++ storage.js
@@ -1,42 +1,64 @@
-const images = {
-    blue: ['images/blueghost1.png', 'images/blueghost2.png'],
-    brown: ['images/brownghost1.png', 'images/brownghost2.png'],
-    red: ['images/redghost1.png', 'images/redghost2.png'],
-    pink: ['images/pinkghost1.png', 'images/pinkghost2.png'],
-    dark: ['images/darkghost1A.png', 'images/darkghost2A.png', 'images/darkghost1B.png', 'images/darkghost2B.png'],
-    pacman: ['images/pacman1.png', 'images/pacman2.png', 'images/pacman3.png']
-};
-const ghosts = [
-    document.getElementById('ghost1'), 
-    document.getElementById('ghost2'),
-    document.getElementById('ghost3'),
-    document.getElementById('ghost4')
-];
-const pacman = document.getElementById('pacman')
+// const images = {
+//     blue: ['images/blueghost1.png', 'images/blueghost2.png'],
+//     brown: ['images/brownghost1.png', 'images/brownghost2.png'],
+//     red: ['images/redghost1.png', 'images/redghost2.png'],
+//     pink: ['images/pinkghost1.png', 'images/pinkghost2.png'],
+//     dark: ['images/darkghost1A.png', 'images/darkghost2A.png', 'images/darkghost1B.png', 'images/darkghost2B.png'],
+//     pacman: ['images/pacman1.png', 'images/pacman2.png', 'images/pacman3.png']
+// };
+// const ghosts = [
+//     document.getElementById('ghost1'), 
+//     document.getElementById('ghost2'),
+//     document.getElementById('ghost3'),
+//     document.getElementById('ghost4')
+// ];
+// const pacman = document.getElementById('pacman')
+// const gameAreaSetup = document.getElementById('GameArea');
+// const game = gameAreaSetup.getContext("2d");
+
+// // let randomvar = 0;
+// function pacManMoves() {
+//     if (randomvar < images.pacman.length) {
+//         pacman.setAttribute("src", images.pacman[randomvar])
+//         randomvar++;
+//     } else if (randomvar === images.pacman.length) {
+//         pacman.setAttribute("src", images.pacman[1]);
+//         randomvar -= 3;
+//     }
+// }
+// // const pacmanmoves = setInterval(pacManMoves, 100); //100 is the sweet spot
+
+// function ghostMoves () {
+//     if (randomvar === 0) {
+//         ghosts[0].setAttribute("src", images.red[0])
+//         randomvar++;
+//     } else if (randomvar === 1) {
+//         ghosts[0].setAttribute("src", images.red[1])
+//         randomvar--;
+//     }
+// }
+// // const ghostMoveTimer = setInterval(ghostMoves, 300); //100 is the sweet spot
+
+const sprites = new Image();
 const gameAreaSetup = document.getElementById('GameArea');
 const game = gameAreaSetup.getContext("2d");
 
-// let randomvar = 0;
-function pacManMoves() {
-    if (randomvar < images.pacman.length) {
-        pacman.setAttribute("src", images.pacman[randomvar])
-        randomvar++;
-    } else if (randomvar === images.pacman.length) {
-        pacman.setAttribute("src", images.pacman[1]);
-        randomvar -= 3;
-    }
+function flipImage() {
+    game.drawImage(sprites, 200, 200, 32, 32)
 }
-// const pacmanmoves = setInterval(pacManMoves, 100); //100 is the sweet spot
 
-function ghostMoves () {
-    if (randomvar === 0) {
-        ghosts[0].setAttribute("src", images.red[0])
-        randomvar++;
-    } else if (randomvar === 1) {
-        ghosts[0].setAttribute("src", images.red[1])
-        randomvar--;
+function flipImage2() {
+    game.translate(200, 200)
+    game.rotate(-90 * Math.PI/180)
+    game.translate(-200, -200)
+    game.drawImage(sprites, 200-32, 200, 32, 32)
     }
-}
-// const ghostMoveTimer = setInterval(ghostMoves, 300); //100 is the sweet spot
 
 
+
+
+
+const timeoutstuff = setTimeout(flipImage, 400)
+const timeoutstuff2 = setTimeout(flipImage2, 400)
+
+sprites.src = "images/pacman2.png";
