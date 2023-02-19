a910045ceb753fdbc3641443281df620c64d8853
joshuapent <joshdavidpent@gmail.com>
2023-02-18 19:30:58 -0600

    Made the sprite list transparent for better interactions

--- pacman.js
+++ pacman.js
@@ -2,11 +2,6 @@ const sprites = new Image();
 const gameAreaSetup = document.getElementById('GameArea');
 const game = gameAreaSetup.getContext("2d");
 
-sprites.onload = function() {
-    game.drawImage(sprites, column*frameWidth, row*frameHeight, frameWidth, frameHeight, 16, 16, frameWidth, frameHeight);
-
-}
-
 // const pacman = {
 //     width: 16,
 //     height: 16,
@@ -16,14 +11,17 @@ sprites.onload = function() {
 
 // game.drawImage(sprites, 3*16, 16, 16, 
 //     16, 10, 30, 16, 16);
-let frameWidth = 12;
-let frameHeight = 13;
+let frameWidth = 16;
+let frameHeight = 16;
 
 // Rows and columns start from 0
 let row = 1;
 let column = 3;
 
+sprites.onload = function() {
+    game.drawImage(sprites, column*frameWidth, row*frameHeight, frameWidth, frameHeight, 16, 16, frameWidth, frameHeight);
 
+}
 
 
 sprites.src = "images/PacManSprites.png";
