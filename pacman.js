e47af6b859b32699511cc5f30a88c45d71683cb7
joshuapent <joshdavidpent@gmail.com>
2023-02-18 19:42:09 -0600

    Sprite sheet was too small, updated

--- pacman.js
+++ pacman.js
@@ -11,17 +11,13 @@ const game = gameAreaSetup.getContext("2d");
 
 // game.drawImage(sprites, 3*16, 16, 16, 
 //     16, 10, 30, 16, 16);
-let frameWidth = 16;
-let frameHeight = 16;
-
-// Rows and columns start from 0
-let row = 1;
-let column = 3;
+let row = 0;
+let column = 0;
 
 sprites.onload = function() {
-    game.drawImage(sprites, column*frameWidth, row*frameHeight, frameWidth, frameHeight, 16, 16, frameWidth, frameHeight);
+    game.drawImage(sprites, column*32, row*32, 32, 32, 200, 100, 32, 32);
 
 }
 
 
-sprites.src = "images/PacManSprites.png";
+sprites.src = "images/Pacman.png";
